0cb8676e46c753e07dc13097051bcd0cf2c4203a
Robert Kieffer <robert@broofa.com>
2023-04-14 07:10:34 -0700

    Add html include rule

--- grammar.js
+++ grammar.js
@@ -1,10 +1,7 @@
 module.exports = grammar({
   name: 'html',
 
-  extras: $ => [
-    $.comment,
-    /\s+/,
-  ],
+  extras: $ => [$.comment, /\s+/],
 
   externals: $ => [
     $._start_tag_name,
@@ -21,96 +18,82 @@ module.exports = grammar({
   rules: {
     fragment: $ => repeat($._node),
 
-    doctype: $ => seq(
-      '<!',
-      alias($._doctype, 'doctype'),
-      /[^>]+/,
-      '>'
-    ),
+    doctype: $ => seq('<!', alias($._doctype, 'doctype'), /[^>]+/, '>'),
 
     _doctype: $ => /[Dd][Oo][Cc][Tt][Yy][Pp][Ee]/,
 
-    _node: $ => choice(
-      $.doctype,
-      $.entity,
-      $.text,
-      $.element,
-      $.script_element,
-      $.style_element,
-      $.erroneous_end_tag
-    ),
-
-    element: $ => choice(
-      seq(
-        $.start_tag,
-        repeat($._node),
-        choice($.end_tag, $._implicit_end_tag)
+    _node: $ =>
+      choice(
+        $.doctype,
+        $.entity,
+        $.text,
+        $.html_include,
+        $.element,
+        $.script_element,
+        $.style_element,
+        $.erroneous_end_tag
       ),
-      $.self_closing_tag
-    ),
 
-    script_element: $ => seq(
-      alias($.script_start_tag, $.start_tag),
-      optional($.raw_text),
-      $.end_tag
-    ),
+    html_include_reference: $ => 'Hello',
 
-    style_element: $ => seq(
-      alias($.style_start_tag, $.start_tag),
-      optional($.raw_text),
-      $.end_tag
-    ),
+    html_include: $ =>
+      seq(token(prec(1, '{{{')), $.html_include_reference, '}}}'),
 
-    start_tag: $ => seq(
-      '<',
-      alias($._start_tag_name, $.tag_name),
-      repeat($.attribute),
-      '>'
-    ),
+    element: $ =>
+      choice(
+        seq(
+          $.start_tag,
+          repeat($._node),
+          choice($.end_tag, $._implicit_end_tag)
+        ),
+        $.self_closing_tag
+      ),
 
-    script_start_tag: $ => seq(
-      '<',
-      alias($._script_start_tag_name, $.tag_name),
-      repeat($.attribute),
-      '>'
-    ),
+    script_element: $ =>
+      seq(
+        alias($.script_start_tag, $.start_tag),
+        optional($.raw_text),
+        $.end_tag
+      ),
 
-    style_start_tag: $ => seq(
-      '<',
-      alias($._style_start_tag_name, $.tag_name),
-      repeat($.attribute),
-      '>'
-    ),
+    style_element: $ =>
+      seq(
+        alias($.style_start_tag, $.start_tag),
+        optional($.raw_text),
+        $.end_tag
+      ),
 
-    self_closing_tag: $ => seq(
-      '<',
-      alias($._start_tag_name, $.tag_name),
-      repeat($.attribute),
-      '/>'
-    ),
+    start_tag: $ =>
+      seq('<', alias($._start_tag_name, $.tag_name), repeat($.attribute), '>'),
 
-    end_tag: $ => seq(
-      '</',
-      alias($._end_tag_name, $.tag_name),
-      '>'
-    ),
+    script_start_tag: $ =>
+      seq(
+        '<',
+        alias($._script_start_tag_name, $.tag_name),
+        repeat($.attribute),
+        '>'
+      ),
 
-    erroneous_end_tag: $ => seq(
-      '</',
-      $.erroneous_end_tag_name,
-      '>'
-    ),
+    style_start_tag: $ =>
+      seq(
+        '<',
+        alias($._style_start_tag_name, $.tag_name),
+        repeat($.attribute),
+        '>'
+      ),
 
-    attribute: $ => seq(
-      $.attribute_name,
-      optional(seq(
-        '=',
-        choice(
-          $.attribute_value,
-          $.quoted_attribute_value
-        )
-      ))
-    ),
+    self_closing_tag: $ =>
+      seq('<', alias($._start_tag_name, $.tag_name), repeat($.attribute), '/>'),
+
+    end_tag: $ => seq('</', alias($._end_tag_name, $.tag_name), '>'),
+
+    erroneous_end_tag: $ => seq('</', $.erroneous_end_tag_name, '>'),
+
+    attribute: $ =>
+      seq(
+        $.attribute_name,
+        optional(seq('=', choice($.attribute_value, $.quoted_attribute_value)))
+      ),
 
     attribute_name: $ => /[^<>"'/=\s]+/,
 
@@ -121,11 +104,12 @@ module.exports = grammar({
     // no more will ever be added.
     entity: $ => /&(#([xX][0-9a-fA-F]{1,6}|[0-9]{1,5})|[A-Za-z]{1,30});/,
 
-    quoted_attribute_value: $ => choice(
-      seq("'", optional(alias(/[^']+/, $.attribute_value)), "'"),
-      seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"')
-    ),
+    quoted_attribute_value: $ =>
+      choice(
+        seq("'", optional(alias(/[^']+/, $.attribute_value)), "'"),
+        seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"')
+      ),
 
-    text: $ => /[^<>&\s]([^<>&]*[^<>&\s])?/
-  }
+    text: $ => /[^<>&\s]([^<>&]*[^<>&\s])?/,
+  },
 });
