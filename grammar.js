faa97de74cbfe8290afda18accc09a382b192502
Alex Vazquez <alex.quez@gmail.com>
2024-11-17 14:46:27 -0800

    Merge with Latest Version of Original HTML Tree Sitter (#3)

--- grammar.js
+++ grammar.js
@@ -5,10 +5,16 @@
  * @license MIT
  */
 
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
 module.exports = grammar({
   name: "html",
 
-  extras: ($) => [$.comment, /\s+/],
+  extras: $ => [
+    $.comment,
+    /\s+/,
+  ],
 
   externals: ($) => [
     $._start_tag_name,
@@ -23,103 +29,122 @@ module.exports = grammar({
   ],
 
   rules: {
-    document: ($) => repeat($._node),
+    document: $ => repeat($._node),
 
-    doctype: ($) => seq("<!", alias($._doctype, "doctype"), /[^>]+/, ">"),
+    doctype: $ => seq(
+      '<!',
+      alias($._doctype, 'doctype'),
+      /[^>]+/,
+      '>',
+    ),
 
-    _doctype: (_) => /[Dd][Oo][Cc][Tt][Yy][Pp][Ee]/,
+    _doctype: _ => /[Dd][Oo][Cc][Tt][Yy][Pp][Ee]/,
 
-    _node: ($) =>
-      choice(
-        $.doctype,
-        $.entity,
-        $.text,
-        $.tribracket,
-        $.element,
-        $.script_element,
-        $.style_element,
-        $.erroneous_end_tag
-      ),
+    _node: $ => choice(
+      $.doctype,
+      $.entity,
+      $.text,
+      $.tribracket,
+      $.element,
+      $.script_element,
+      $.style_element,
+      $.erroneous_end_tag,
+    ),
 
     // We have to wrap the opening '{{{' token in `token(prec())` calls here to
     // force the lexer to give precedence to this rule over the `text` rule. I'm
     // not 100% how this works, but this was suggested by @ahlinc in this
     // Discord thread:
     // https://discord.com/channels/1063097320771698699/1063097321648312354
-    tribracket: ($) => seq(token(prec(1, "{{{")), $.tribracket_content, "}}}"),
+    tribracket: ($) => seq(token(prec(1, '{{{')), $.tribracket_content, '}}}'),
     tribracket_content: ($) => /[^}]*/,
 
-    element: ($) =>
-      choice(
-        seq(
-          $.start_tag,
-          repeat($._node),
-          choice($.end_tag, $._implicit_end_tag)
+    element: $ => choice(
+      seq(
+        $.start_tag,
+        repeat($._node),
+        choice($.end_tag, $._implicit_end_tag),
+      ),
+      $.self_closing_tag,
+    ),
+
+    script_element: $ => seq(
+      alias($.script_start_tag, $.start_tag),
+      optional($.raw_text),
+      $.end_tag,
+    ),
+
+    style_element: $ => seq(
+      alias($.style_start_tag, $.start_tag),
+      optional($.raw_text),
+      $.end_tag,
+    ),
+
+    start_tag: $ => seq(
+      '<',
+      alias($._start_tag_name, $.tag_name),
+      repeat($.attribute),
+      '>',
+    ),
+
+    script_start_tag: $ => seq(
+      '<',
+      alias($._script_start_tag_name, $.tag_name),
+      repeat($.attribute),
+      '>',
+    ),
+
+    style_start_tag: $ => seq(
+      '<',
+      alias($._style_start_tag_name, $.tag_name),
+      repeat($.attribute),
+      '>',
+    ),
+
+    self_closing_tag: $ => seq(
+      '<',
+      alias($._start_tag_name, $.tag_name),
+      repeat($.attribute),
+      '/>',
+    ),
+
+    end_tag: $ => seq(
+      '</',
+      alias($._end_tag_name, $.tag_name),
+      '>',
+    ),
+
+    erroneous_end_tag: $ => seq(
+      '</',
+      $.erroneous_end_tag_name,
+      '>',
+    ),
+
+    attribute: $ => seq(
+      $.attribute_name,
+      optional(seq(
+        '=',
+        choice(
+          $.attribute_value,
+          $.quoted_attribute_value,
         ),
-        $.self_closing_tag
-      ),
+      )),
+    ),
 
-    script_element: ($) =>
-      seq(
-        alias($.script_start_tag, $.start_tag),
-        optional($.raw_text),
-        $.end_tag
-      ),
+    attribute_name: _ => /[^<>"'/=\s]+/,
 
-    style_element: ($) =>
-      seq(
-        alias($.style_start_tag, $.start_tag),
-        optional($.raw_text),
-        $.end_tag
-      ),
-
-    start_tag: ($) =>
-      seq("<", alias($._start_tag_name, $.tag_name), repeat($.attribute), ">"),
-
-    script_start_tag: ($) =>
-      seq(
-        "<",
-        alias($._script_start_tag_name, $.tag_name),
-        repeat($.attribute),
-        ">"
-      ),
-
-    style_start_tag: ($) =>
-      seq(
-        "<",
-        alias($._style_start_tag_name, $.tag_name),
-        repeat($.attribute),
-        ">"
-      ),
-
-    self_closing_tag: ($) =>
-      seq("<", alias($._start_tag_name, $.tag_name), repeat($.attribute), "/>"),
-
-    end_tag: ($) => seq("</", alias($._end_tag_name, $.tag_name), ">"),
-
-    erroneous_end_tag: ($) => seq("</", $.erroneous_end_tag_name, ">"),
-
-    attribute: ($) =>
-      seq(
-        $.attribute_name,
-        optional(seq("=", choice($.attribute_value, $.quoted_attribute_value)))
-      ),
-
-    attribute_name: (_) => /[^<>"'/=\s]+/,
-
-    attribute_value: (_) => /[^<>"'=\s]+/,
+    attribute_value: _ => /[^<>"'=\s]+/,
 
     // An entity can be named, numeric (decimal), or numeric (hexacecimal). The
     // longest entity name is 29 characters long, and the HTML spec says that
     // no more will ever be added.
-    entity: (_) => /&(#([xX][0-9a-fA-F]{1,6}|[0-9]{1,5})|[A-Za-z]{1,30});?/,
+    entity: _ => /&(#([xX][0-9a-fA-F]{1,6}|[0-9]{1,5})|[A-Za-z]{1,30});?/,
 
-    quoted_attribute_value: ($) =>
-      choice(
-        seq("'", optional(alias(/[^']+/, $.attribute_value)), "'"),
-        seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"')
-      ),
+    quoted_attribute_value: $ => choice(
+      seq('\'', optional(alias(/[^']+/, $.attribute_value)), '\''),
+      seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"'),
+    ),
 
-    text: (_) => /[^<>&\s]([^<>&]*[^<>&\s])?/,
+    text: _ => /[^<>&\s]([^<>&]*[^<>&\s])?/,
   },
 });
